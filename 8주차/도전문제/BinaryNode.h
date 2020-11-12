bede90477c72102b43daf252e74984c5b3fef87d
zzanguna <zzanguna>
2020-11-12 23:31:25 +0900

    8주차 자료구조

---
+++ 8주차/도전문제/BinaryNode.h
@@ -0,0 +1,26 @@
+//
+// Created by 짱유나 on 2020/11/12.
+//
+
+#pragma once
+#include <stdio.h>
+
+class BinaryNode{
+protected:
+    int			data;
+    BinaryNode	*left;
+    BinaryNode	*right;
+public:
+    BinaryNode( int val=0, BinaryNode *l=NULL, BinaryNode *r=NULL)
+            : data(val), left(l), right(r) { }
+    ~BinaryNode()	{ }
+
+    void		setData ( int val )		{ data	= val; }
+    void		setLeft (BinaryNode *l)	{ left	= l; }
+    void		setRight(BinaryNode *r)	{ right	= r; }
+    int			getData ()				{ return data; }
+    BinaryNode*	getLeft ()				{ return left; }
+    BinaryNode*	getRight()				{ return right; }
+    bool		isLeaf()	{ return left==NULL && right==NULL; }
+};
+
